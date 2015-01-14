fcced6e016f9ff428bd0eba05fa7908eabe6d238
AmiDzxc <amidzxc@mail.ru>
2015-01-14 13:31:44 +0200

    AmiD изменил дизайн

--- app/src/main/java/com/zfakgroup/israel/schoollocker/FragmentSignUp.java
+++ app/src/main/java/com/zfakgroup/israel/schoollocker/FragmentSignUp.java
@@ -100,7 +100,7 @@ public class FragmentSignUp extends Fragment {
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
-     */
+     *///
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
