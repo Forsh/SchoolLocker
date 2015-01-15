4d023492e6bf5cd31600e176f563c00fddad8806
AmiDzxc <amidzxc@mail.ru>
2015-01-15 14:59:27 +0200

    AmiD изменил дизайн

--- app/src/main/java/com/zfakgroup/israel/schoollocker/MainActivity.java
+++ app/src/main/java/com/zfakgroup/israel/schoollocker/MainActivity.java
@@ -3,6 +3,7 @@ package com.zfakgroup.israel.schoollocker;
 import android.app.FragmentManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends FragmentActivity
@@ -28,13 +30,19 @@ public class MainActivity extends FragmentActivity
         // Главный экран - контейнер фрагментов во весь экран
         setContentView(R.layout.fullscreen);
 
+
+
         //Interface for interacting with Fragment objects inside of an Activity
         //Добавляет и убирает фрагменты с Activity
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().add(R.id.fragment_container,new FragmentLogIn()).commit();
+       FragmentManager fragmentManager = getFragmentManager();
+       fragmentManager.beginTransaction().add(R.id.fragment_container,new FragmentLogIn()).commit();
         // Обращение к базе данных осуществляется через интерфейс.
         IServiceConnect connect;
+
+
     };
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
