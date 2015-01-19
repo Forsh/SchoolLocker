f23ea1dad7287080a8e12c9a7d9e119035971b79
mac <forshtat@bk.ru>
2015-01-19 20:46:17 +0200

    Запросы к базе выполняются, результаты приходят

---
+++ backend/src/main/java/com/example/mac/myapplication/backend/Course.java
@@ -0,0 +1,92 @@
+package com.example.mac.myapplication.backend;
+
+
+import java.util.Date;
+
+/**
+ * Created by mac on 14.01.15.
+ */
+public class Course{
+
+
+
+    private String name;
+    private String description;
+    private Date begin;
+    private Date end;
+
+    private String[] files;
+    private String[] folders;
+    private String[] links;
+
+    public Course(){
+
+    }
+
+    public Course(String name, String description, Date begin, Date end, String[] files, String[] folders, String[] links) {
+        this.name = name;
+        this.description = description;
+        this.begin = begin;
+        this.end = end;
+        this.files = files;
+        this.folders = folders;
+        this.links = links;
+    }
+
+    public String[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(String[] files) {
+        this.files = files;
+    }
+
+    public String[] getFolders() {
+        return folders;
+    }
+
+    public void setFolders(String[] folders) {
+        this.folders = folders;
+    }
+
+    public String[] getLinks() {
+        return links;
+    }
+
+    public void setLinks(String[] links) {
+        this.links = links;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Date begin) {
+        this.begin = begin;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}
---
+++ backend/src/main/java/com/example/mac/myapplication/backend/Group.java
@@ -0,0 +1,80 @@
+package com.example.mac.myapplication.backend;
+
+import java.util.Date;
+
+/**
+ * Created by mac on 14.01.15.
+ *
+ * Заглушка для класса группы.
+ *
+ */
+
+public class Group{
+
+    private String name;
+    private String description;
+    private Date begin;
+    private Date end;
+    private Course[] courses;
+    private User[] users;
+
+    public Group()
+    {}
+
+    public Group(String name, String description, Date begin, Date end, User[] users) {
+        this.name = name;
+        this.description = description;
+        this.begin = begin;
+        this.end = end;
+        this.courses = courses;
+        this.users = users;
+    }
+
+    public Course[] getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Course[] courses) {
+        this.courses = courses;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public void setUsers(User[] users) {
+        this.users = users;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Date begin) {
+        this.begin = begin;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}
